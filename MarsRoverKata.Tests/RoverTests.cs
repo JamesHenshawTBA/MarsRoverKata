2e6f3b3983b243dbaea317d319f3318dfed873be
James Henshaw <james.henshaw@tba.group>
2025-04-09 12:20:46 +0100

    Added test for init rover

---
+++ MarsRoverKata.Tests/RoverTests.cs
@@ -0,0 +1,24 @@
+namespace MarsRoverKata.Tests;
+
+public class RoverTests
+{
+    [Test]
+    public void InitialiseRoverFacingThisDirection()
+    {
+        const string expectedDirection = "NORTH";
+
+        var rover = new Rover(expectedDirection);
+
+        Assert.That(rover.Direction, Is.EqualTo(expectedDirection));
+    }
+}
+
+public class Rover
+{
+    public string Direction { get; }
+
+    public Rover(string direction)
+    {
+        Direction = direction;
+    }
+}
