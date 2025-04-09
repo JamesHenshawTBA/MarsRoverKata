801c4cbe0013693bce150337b4fef6d054d5a781
James Henshaw <james.henshaw@tba.group>
2025-04-09 12:27:59 +0100

    Added test for init with X & Y coordinates

--- MarsRoverKata.Tests/RoverTests.cs
+++ MarsRoverKata.Tests/RoverTests.cs
@@ -7,8 +7,20 @@ public class RoverTests
     {
         const string expectedDirection = "NORTH";
 
-        var rover = new Rover(expectedDirection);
+        var rover = new Rover(expectedDirection, 0, 0);
 
         Assert.That(rover.Direction, Is.EqualTo(expectedDirection));
+    }
+
+    [Test]
+    public void InitialiseRoverWithTheseCoordinates()
+    {
+        const int xCoordinate = 2;
+        const int yCoordinate = 4;
+
+        var rover = new Rover("NORTH", xCoordinate, yCoordinate);
+
+        Assert.That(rover.XCoordinate, Is.EqualTo(xCoordinate));
+        Assert.That(rover.YCoordinate, Is.EqualTo(yCoordinate));
     }
 }
--- MarsRoverKata/Rover.cs
+++ MarsRoverKata/Rover.cs
@@ -3,9 +3,13 @@ namespace MarsRoverKata;
 public class Rover
 {
     public string Direction { get; }
+    public int XCoordinate { get; }
+    public int YCoordinate { get; }
 
-    public Rover(string direction)
+    public Rover(string direction, int xCoordinate, int yCoordinate)
     {
         Direction = direction;
+        XCoordinate = xCoordinate;
+        YCoordinate = yCoordinate;
     }
 }
