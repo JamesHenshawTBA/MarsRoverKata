5b62a75b654e65d9600eec7e1f3d8f3419dbd3b3
James Henshaw <james.henshaw@tba.group>
2025-04-09 12:02:34 +0100

    Added skeleton projects

---
+++ MarsRoverKata.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MarsRoverKata.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
